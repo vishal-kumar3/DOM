9b801783bdf91f21aeb79cff07f486186fd98911
Vishal Kumar <121259738+vishal-kumar3@users.noreply.github.com>
2023-09-04 17:58:34 +0530

    Spotify html,css Done... JS Pending.

--- multiple_elem_hover/script.js
+++ multiple_elem_hover/script.js
@@ -5,8 +5,13 @@ elem.forEach(function(element) {
 
     element.childNodes[1].addEventListener("mousemove", function(dest){
         // img.style.transform = "scale(1)";
+        var rect = element.getBoundingClientRect(); // to get position of parent element
+
+        console.log("parent Element position:- ", rect.top);
+        console.log("cursor position:- ", dest.y);
+
         element.childNodes[3].style.left = dest.x + "px";
-        // element.childNodes[3].style.top = dest.y + "px";
+        element.childNodes[3].style.top = (dest.y - rect.top) + "px";
         // img.style.transform = "scale(0)";
     });
     
